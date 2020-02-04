9c25f9d68015df4793a7238c0ee323d970db43a5
greenmac <alwaysmac@msn.com>
2020-02-04 16:41:42 +0800

    2.2_ndarray

--- 2.2_ndarray.py
+++ 2.2_ndarray.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import *
 
 def main():
     # 1
@@ -25,7 +26,46 @@ def main():
     print(np.random.choice([10, 20, 30, 2, 8]))
     print('Distribute:')
     print(np.random.beta(1, 10, 100))
-    
+    # 3 Array Opes
+    lst = np.arange(1, 11).reshape([2, -1])
+    print('Exp:', np.exp(lst))
+    print('Exp2:', np.exp2(lst))
+    print('Sqrt:', np.sqrt(lst))
+    print('Sin:', np.sin(lst))
+    print('Log:', np.log(lst))
 
+    lst = np.array([
+        [[1, 2, 3, 4], [4, 5, 6, 7]],
+        [[7, 8 , 9, 10], [10, 11, 12, 13]],
+        [[14, 15, 16, 17], [18, 19, 20, 21]]
+    ])
+    print('axis_0:\n', lst.sum(axis=0))
+    print('axis_1:\n', lst.sum(axis=1))
+    print('axis_2:\n', lst.sum(axis=2))
+    print('axis_1_max:\n', lst.max(axis=1))
+    print('axis_0_min:\n', lst.min(axis=0))
+    
+    lst1 = np.array([10, 20, 30, 40])
+    lst2 = np.array([4, 3, 2, 1])
+    print('Add:', lst1 + lst2)
+    print('Dot:', np.dot(lst1.reshape([2, 2]), lst2.reshape([2, 2])))
+    
+    print('Concatenate:', np.concatenate((lst1, lst2), axis=0))
+    print('Vstack:', np.vstack((lst1, lst2)))
+    print('Hstack:', np.hstack((lst1, lst2)))
+    print('Split_2:', np.split(lst1, 2))
+    print('Split_4:', np.split(lst1, 4))
+    print('Copy:', np.copy(lst1, 4))
+    
+    # 4 liner
+    print('Eye:\n', np.eye(3))
+    lst = np.array([
+        [1, 2],
+        [3, 4]
+        ])
+    print('Inv:\n', inv(lst))
+    print('T:\n', lst.transpose())
+    print('Det:\n', det(lst))
+    
 if __name__ == '__main__':
     main()
